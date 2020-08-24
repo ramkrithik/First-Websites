100512da067848d1f3caf84664362e64519c11b7
Ramkrithik S <sugeeram005@gmail.com>
2020-08-24 20:25:03 +0530

    finally done

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const game = () => {
     let pscore = 0;
-    let csore = 0;
+    let cscore = 0;
     const startgame = () => {
         const playbtn = document.querySelector('.intro button');
         const introscreen = document.querySelector('.intro');
@@ -14,10 +14,64 @@ const game = () => {
         const options = document.querySelectorAll('.options button');
         const playerhand = document.querySelector('.player-hand');
         const comphand = document.querySelector('.computer-hand');
-        const compopt = ['rock', 'paper', 'scissors'];
-        const compnum = Math.floor(Math.random() * 3);
-        console.log(compnum);
+        const compopt = ['rock', 'paper', 'scissor'];
+        const hands = document.querySelectorAll('.hands img');
+        hands.forEach(hand => {
+            hand.addEventListener('animationend', function() {
+                this.style.animation = '';
+            })
+        });
+        options.forEach((option) => {
+            option.addEventListener('click', function() {
+                const compnum = Math.floor(Math.random() * 3);
+                const compchoice = compopt[compnum];
 
+                setTimeout(() => {
+                    comphands(this.textContent, compchoice);
+                    playerhand.src = `./assets/${this.textContent}.png`;
+                    comphand.src = `./assets/${compchoice}.png`;
+                }, 1000)
+                playerhand.style.animation = 'shakeplayer 1s ease';
+                comphand.style.animation = 'shakecomp 1s ease';
+            });
+        });
+
+    };
+    const comphands = (pchoice, cchoice) => {
+        const winner = document.querySelector('.winner')
+        if (pchoice === cchoice) {
+            winner.textContent = 'It is a tie';
+            updatescore();
+            return;
+        } else if (pchoice === 'rock' && cchoice === 'scissor') {
+            winner.textContent = 'player is winner';
+            pscore++;
+            updatescore();
+            return;
+        } else if (pchoice === 'paper' && cchoice === 'rock') {
+            winner.textContent = 'player is winner';
+            pscore++;
+            updatescore();
+            return;
+        } else if (pchoice === 'scissor' && cchoice === 'paper') {
+            winner.textContent = 'player is winner';
+            pscore++;
+            updatescore();
+            return;
+        } else {
+            winner.textContent = 'computer is winner';
+            cscore++;
+            updatescore();
+            return;
+        }
+
+
+    };
+    const updatescore = () => {
+        const playerscore = document.querySelector('.player-score p');
+        const compscore = document.querySelector('.computer-score p');
+        playerscore.textContent = pscore;
+        compscore.textContent = cscore;
     };
     startgame();
     playmatch();
